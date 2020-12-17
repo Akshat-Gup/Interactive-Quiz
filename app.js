a253474b4a7a11a7de2ccb9e74db5675b5fd58f5
Akshat-Gup <69629823+Akshat-Gup@users.noreply.github.com>
2020-12-17 22:04:00 +0800

    updating code

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const correctAnswers = ["B","A","B","B"];
+//Variable Initialisation
+const correctAnswers = ["A","A","A","A"]; 
 const userForm = document.querySelector(".quiz-form");
 const resultBlock = document.querySelector(".result");
 const scoreHTML = document.querySelector(".score");
-const errorBlock = document.querySelector(".errorBlock")
+const errorBlock = document.querySelector(".errorBlock");
+const quizSection = document.querySelector("body > div.quiz.w-70.m-5.shadow.bg-white.rounded.my-0");
+const nextSteps = document.querySelector(".next");
+
 userForm.addEventListener("submit", event => {
 	event.preventDefault();
 	let userScore = 0;
-	const maxScore = 4;
+	const maxPossibleScore = 4;
 	const userOptionsSelected = [userForm.q1.value,userForm.q2.value,userForm.q3.value,userForm.q4.value];
-	console.log(userOptionsSelected);
 	userOptionsSelected.forEach((answer, index) => {
 		if (answer === "") {
-			scrollTo(0,0);
+			scrollTo({
+				top: 0,
+				behavior: "smooth"
+			});
 			errorBlock.classList.remove("d-none");
 		} else if (answer === correctAnswers[index]) {
 			userScore+=1;
@@ -19,16 +25,21 @@ userForm.addEventListener("submit", event => {
 	});
 	if (errorBlock.classList.contains("d-none")) {
 		scrollTo(0,0);
-		userScore = Math.round(userScore/maxScore*100); 
+		userScore = Math.round(userScore/maxPossibleScore*100); 
 		resultBlock.classList.remove("d-none");
 		let countUp = 0;
 		let displayScore = setInterval(() => {
 			scoreHTML.innerText = countUp;
 			if (countUp===userScore) {
+				flag = true;
 				clearInterval(displayScore);
 			} else {
 				countUp++;
 			}
 		},20);
+		nextSteps.classList.remove("d-none");
+		quizSection.classList.add("d-none");
+
+		
 	}
 });
